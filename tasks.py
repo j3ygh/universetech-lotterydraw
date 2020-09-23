f7ea96ba73f64b6bc9b9d90c67aeecca6d0577f3
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2020-09-23 17:56:43 +0800

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,6 +1,17 @@
 from invoke import task
 from pathlib import Path
+import platform
 import shutil
+
+
+OS = platform.system().lower()
+BASE_DIR = Path(__file__).parent
+PYTHON_VENV_DIR_NAME = '.venv'
+PATHON_PATHS = {
+    'windows': BASE_DIR / PYTHON_VENV_DIR_NAME / 'Scripts' / 'python.exe',
+    'linux': BASE_DIR / PYTHON_VENV_DIR_NAME / 'bin' / 'python',
+    'darwin': BASE_DIR / PYTHON_VENV_DIR_NAME / 'bin' / 'python',
+}
 
 
 @task
@@ -9,3 +20,8 @@ def d4vscode(c):
     file1 = basedir / '.vscode' / 'd4settings.json'
     file2 = basedir / '.vscode' / 'settings.json'
     shutil.copy(file1, file2)
+
+
+@task
+def demo(c, docs=False):
+    c.run(f'{PATHON_PATHS[OS]} {BASE_DIR / "lotterydraw" / "manage.py"} runserver')
