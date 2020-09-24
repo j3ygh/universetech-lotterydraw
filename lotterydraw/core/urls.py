68f36f033ede4c98ce696c41e4aa6f56f616a414
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2020-09-24 14:26:16 +0800

    feat✨(demo): add feature demo

--- lotterydraw/core/urls.py
+++ lotterydraw/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.views.generic import RedirectView
-from .views import lottery_list
+from .views import lottery_list, demo
 
 
 urlpatterns = [
-    path('lotteries/', lottery_list),
-    path('', RedirectView.as_view(url='lotteries/')),
+    path('lotteries/', lottery_list, name='lottery_list'),
+    path('demo/', demo, name='demo'),
+    path('', RedirectView.as_view(url='demo/')),
 ]
--- lotterydraw/core/views.py
+++ lotterydraw/core/views.py
@@ -1,5 +1,9 @@
 from .models import Lottery
 from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+import requests
+from django.core.management import call_command
 
 
 def lottery_list(request):
@@ -13,3 +17,39 @@ def lottery_list(request):
         'data': data,
     }
     return JsonResponse(results)
+
+
+def demo(request):
+    if request.method == 'POST':
+        action = request.POST.get('action')
+        if action == 'create':
+            call_command('createlottery')
+        elif action == 'update':
+            call_command('updatelottery')
+        elif action == 'delete':
+            call_command('deletelottery')
+        return redirect(reverse('demo'))
+    # lotteries
+    objects_list = Lottery.objects.all()
+    data = [{
+        'game_id': obj.game_id,
+        'issue': obj.issue,
+        'winning_number': obj.winning_number,
+    } for obj in objects_list]
+    results = {
+        'data': data,
+    }
+    # onefake
+    url1 = 'http://127.0.0.1:8001/v1'
+    r1 = requests.get(url1)
+    results1 = r1.json()['result']
+    # twofake
+    url2 = 'http://127.0.0.1:8002/newly.do/'
+    r2 = requests.get(url2)
+    results2 = r2.json()
+    context = {
+        'results': results,
+        'results1': results1,
+        'results2': results2,
+    }
+    return render(request, 'core/lottery_list.html', context)
